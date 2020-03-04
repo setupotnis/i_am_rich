07020db2e14e12e2babcf1bebf93f70b35de888b
Setu Potnis <setupotnis@gmail.com>
2020-03-04 10:57:33 -0500

    Customized color

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
           title: Text("I am Rich"),
         ),
       ),
