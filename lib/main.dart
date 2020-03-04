20795b0a8b2507e256e96c8428af18df371a270c
Setu Potnis <setupotnis@gmail.com>
2020-03-04 11:11:36 -0500

    added a centered image widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,11 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9')),
+        ),
         appBar: AppBar(
           backgroundColor: Colors.blueGrey[900],
           title: Text("I am Rich"),
